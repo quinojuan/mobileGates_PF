04134c1abfed60ba45e068d9e1dcb8596d3054aa
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-09-13 13:06:51 -0300

    Lautaro 4to Commit

---
+++ api/src/controllers/usersController.js
@@ -0,0 +1,4 @@
+const getAll = async() =>{
+    // Código asíncrono para traer toda la información (todos los usuarios)
+    // Primera ruta a armar para que los chicos del front puedan trabajar 
+};
