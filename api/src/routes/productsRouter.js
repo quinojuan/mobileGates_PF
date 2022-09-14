5a3ea1eb2fcf290e9582b4e6c7066b0a4e0b2b72
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-09-14 10:17:33 -0300

    12vo commit Lautaro

--- api/src/routes/productsRouter.js
+++ api/src/routes/productsRouter.js
@@ -7,12 +7,7 @@ const {
 } = require('../controllers/productsController');
 
 router.get('/', getAllProducts);
-<<<<<<< HEAD
 router.get('/tablets', getAllTablets);
 router.post('/', postTablet);
-=======
-router.get('/tablets', getAllTablets); // >>>>>>> está apuntando a traer los telefonos (IMSOMNIA) <<<<<<<
-router.post('/', postProduct);
->>>>>>> 3a8a0ac82ddbe3a25c5c8bc058ce2e3c44562b79
 
 module.exports = router;
