a1918da3cf703c0b8b323741cd421d1f00c01064
matiaskoziupa <matikoziupa@gmail.com>
2022-09-14 15:55:10 -0300

    pf in proccess

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,10 +1,14 @@
-import React from "react"; 
+import React from "react";
+import {Link} from "react-router-dom" 
 
-export default function Cards({model, image}) {
+export default function Cards({model, image, brand}) {
     return (
         <div>
+          
             <img src={image} alt='Image not found' width='200px' height='250px'></img>
-            <h3>{model}</h3> 
+            
+            <h3>{model}</h3>
+            <h5>{brand}</h5> 
         </div>
     )
 }
--- client/src/components/Details/DetailsPhone.jsx
+++ client/src/components/Details/DetailsPhone.jsx
@@ -1,33 +1,33 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import {useSelector, useDispatch} from "react-redux";
-// import { getNotebooksById, getPhonesById, getTabletsById } from "../../redux/Actions";
-// import {Link, useHistory, useParams} from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import {useSelector, useDispatch} from "react-redux";
+import { getPhonesById, getClean } from "../../redux/Actions";
+import {Link, useParams} from "react-router-dom";
 
-// export default function Details(props){
-//     const dispatch= useDispatch()
-//     const history=useHistory()
-//     const {id}= useParams
+export default function DetailsPhone(props){
+    const dispatch= useDispatch()
+    const {id}= useParams()
 
-//     useEffect(()=>{
-//         dispatch(getPhonesById(props.match.params.id))
-//     }, [dispatch])
-//     const myProducts=useSelector((state)=>state.details)
-//     return(
-//         <div>
-//             <Link to="/">Back</Link>
-//             {
-//                 myProducts && myProducts.length > 0 ?
-//                     <div>
-//                         <h1>{myProducts.model}</h1>
-//                         <h3>Category: {myProducts.category}</h3>
-//                         <h3>Dish types: {myProducts.brand}</h3>
-//                         <h3> Colors: {myProducts.colors}</h3>
-//                         <h5 >Capacity: { myProducts[0].capacity}</h5>
-//                         <img src={myProducts.image} alt="Not found" width="200px" height="250px"></img>
-//                         <h6>{myProducts.summary} </h6>
-//                     </div> : <div><div><h4>Loading...</h4></div></div>
-//             }
-//         </div>
-//     )
-// }
+    useEffect(()=>{
+        dispatch(getPhonesById(id))
+        dispatch(getClean())
+    }, [dispatch])
+    const myProducts=useSelector((state)=>state.details)
+    console.log(myProducts)
+    return(
+        <div>
+            <Link to="/home">Back</Link>
+            {
+                
+                    <div>
+                        <h1>{myProducts&&myProducts.model}</h1>
+                        <h3>Category: {myProducts&&myProducts.category}</h3>
+                        <h3>Brand: {myProducts&&myProducts.brand}</h3>
+                        <h5 >Capacity: { myProducts&&myProducts.capacity}</h5>
+                        <img src={myProducts&&myProducts.image} alt="Not found" width="200px" height="250px"></img>
+                        <h6>{myProducts&&myProducts.description} </h6>
+                    </div> 
+            }
+        </div>
+    )
+}
