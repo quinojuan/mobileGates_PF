317f3efe9de57a1e0e17d7a1c4ed8f9c25b2a50a
matiaskoziupa <matikoziupa@gmail.com>
2022-09-14 11:56:03 -0300

    details in procces

--- client/src/redux/Actions/index.js
+++ client/src/redux/Actions/index.js
@@ -9,3 +9,43 @@ export function getAllProducts(){
         })
     }
 }
+export function getPhonesById(id){
+    return async function (dispatch){
+        try{
+            let json= await axios.get(`http://localhost:3001/products/phones/${id}`)
+            return dispatch({
+                type: "GET_PHONES_BY_ID",
+                payload: json
+        })
+        } catch(error){
+            console.log(error)
+        }
+    }
+}
+export function getTabletsById(id){
+    return async function (dispatch){
+        try{
+            let json= await axios.get(`http://localhost:3001/products/tablets/${id}`)
+            return dispatch({
+                type:"GET_TABLETS_BY_ID",
+                payload:json
+            })
+        } catch(error){
+            console.log(error)
+        }
+       
+    }
+}
+export function getNotebooksById(id){
+    return async function (dispatch){
+        try{
+            let json= await axios.get(`http://localhost:3001/products/notebooks/${id}`)
+            return dispatch({
+                type:"GET_NOTEBOOKS_BY_ID",
+                payload:json
+        })
+        } catch(error){
+            console.log(error)
+        }
+    }
+}
--- client/src/redux/Reducer/index.js
+++ client/src/redux/Reducer/index.js
@@ -1,5 +1,6 @@
 const initialState = {
-    products: []
+    products: [],
+    details:[]
 }
 
 function rootReducer(state = initialState, action) {
@@ -9,6 +10,21 @@ function rootReducer(state = initialState, action) {
                 ...state,
                 products: action.payload,
             }
+            case "GET_PHONES_BY_ID":
+                return {
+                    ...state,
+                    details: action.payload
+                }
+                case "GET_TABLETS_BY_ID":
+                    return {
+                        ...state,
+                        details:action.payload
+                    }
+                    case "GET_NOTEBOOKS_BY_ID":
+                        return{
+                            ...state,
+                            details:action.payload
+                        }
         default:
             return state;
     }
