dd30f6fdc4b66c521bd49e3de140ac8e8153a440
matiaskoziupa <matikoziupa@gmail.com>
2022-09-14 20:16:46 -0300

    Search bar in proccess

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
 import { searchName } from '../../redux/Actions/index'
+import { useState } from "react";
 
-export default function SearchBar() {
+export default function SearchBar({setCurrentPage, setProductsPerPage }) {
 
     const dispatch = useDispatch()
+    const [name, setName]=useState("")
   
-    const handleInputChange = (e) => {
-        dispatch(searchName(e.target.value))
+    function handleInputChange(e){
+        e.preventDefault();
+        setName(e.target.value)
+    }
+    function handleSubmit(e){
+        e.preventDefault()
+        if(name.length!==0){
+            dispatch(searchName(name.toLowerCase()))
+        } else {
+            alert("Not found")
+        }
+        setCurrentPage(1)
+        setName("")
     }
 
     return (
         <div>
-            <h2>Qué producto estas buscando?</h2>
-            <input type="text" onChange={handleInputChange} placeholder="Buscá un producto..."/> 
+            <h2>What are you looking for?</h2>
+            <input type="text" placeholder="Search..." onChange={(e)=>handleInputChange(e)} /> 
+            <button onClick={(e)=>handleSubmit(e)}>Search</button>
         </div>
     )
 
