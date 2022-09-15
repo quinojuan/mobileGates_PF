1f524cbfed4ddcebc1917be00286c3ddd16ea9d2
LautaroOlmedo <72723456+LautaroOlmedo@users.noreply.github.com>
2022-09-15 09:46:57 -0300

    22vo commit Lautaro

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -1,4 +1,28 @@
-const getAll = async() =>{
+const {User} = require('../db.js')
+
+
+const getAllUsers = async(req, res) =>{
     // Código asíncrono para traer toda la información (todos los usuarios)
     // Primera ruta a armar para que los chicos del front puedan trabajar 
+    const allUsers = await User.findAll();
+    res.status(200).json(allUsers)
+};
+
+const getUserById = async(req, res) =>{
+    // Código asíncrono para traer toda la información (todos los usuarios)
+    // Primera ruta a armar para que los chicos del front puedan trabajar 
+    const {id} = req.params;
+
+
+    const user = await User.findByPk(id)
+    res.status(200).json(user)
+};
+
+const createUser = async(req, res) =>{
+    // Código asíncrono para traer toda la información (todos los usuarios)
+    // Primera ruta a armar para que los chicos del front puedan trabajar 
+    const {name, email, password} = req.body;
+
+    const user = await User.create({where: {name, email, password}})
+    res.status(200).json(user)
 };
