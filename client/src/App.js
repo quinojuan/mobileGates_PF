e551e6d049002df6e4657dd15e3698c632966656
giuliano-627 <f15ur4.97@gmail.com>
2022-09-15 13:42:43 -0300

    Terminado el redirect

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import DetailsNotebook from "./components/Details/DetailsNotebook";
 import DetailsPhone from './components/Details/DetailsPhone';
 import DetailsTablet from './components/Details/DetailsTablet';
+import Landing from './components/Landing/Landing';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Switch>
+        <Route exact path = '/' component = {Landing}/>
         <Route exact path = '/home' component = {Home}/>
         <Route path="/products/notebooks/:id" component= {DetailsNotebook}/>
         <Route path="/products/phones/:id" component= {DetailsPhone}/>
