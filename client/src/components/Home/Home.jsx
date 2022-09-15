e73ebbca09a5d6a1ade029d15ad5e47fef2afc33
matiaskoziupa <matikoziupa@gmail.com>
2022-09-14 21:07:40 -0300

    home renderizado

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -34,6 +34,40 @@ export default function Home() {
         <div>
         <div>
             <NavBar/>
+        </div>
+        <div>
+        <select>
+        <option value="disabled">Filter by RAM</option>
+        <option value="ram2">2 GB</option>
+        <option value="ram3">3 GB</option>
+        <option value="ram4">4 GB</option>
+        <option value="ram6">6 GB</option>
+        <option value="ram8">8 GB</option>
+        <option value="ram16">16 GB</option>
+        <option value="ram32">32 GB</option>
+        <option value="ram64">64 GB</option>
+        </select>
+        <select>
+        <option value="disabled">Almacenamiento</option>
+        <option value="GB32">32 GB</option>
+        <option value="GB64">64 GB</option>
+        <option value="GB120">120 GB</option>
+        <option value="GB128">128 GB</option>
+        <option value="GB240">240 GB</option>
+        <option value="GB256">256 GB</option>
+        <option value="GB400">400 GB</option>
+        <option value="GB480">480 GB</option>
+        <option value="TB1">1 TB</option>
+        
+        </select>
+        <select>
+        <option value="disabled">Categoria</option>
+        <option value="notebooks">Notebooks</option>
+        <option value="tablets">Tablets</option>
+        <option value="celulares">Celulares</option>
+        </select>
+        </div>
+        <div>
             <SearchBar
             setCurrentPage={setCurrentPage}
             setProductsPerPage={setProductsPerPage}
