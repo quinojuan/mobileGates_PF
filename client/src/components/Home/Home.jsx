9e63468e7d56faa89b6ae5eae1f3028032867064
Alejandro Diaz <adiazcjc@gmail.com>
2022-09-18 21:50:27 -0300

    sumando estilos

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-	getAllProducts,
-	getSort,
-	setFilter,
-	getProductsByNameAndFilters,
-	getCart
+  getAllProducts,
+  getSort,
+  setFilter,
+  getProductsByNameAndFilters,
+  getCart
 } from '../../redux/Actions/index';
 import Cards from '../Cards/Cards';
 import NavBar from '../NavBar/NavBar';
@@ -15,7 +15,7 @@ import Footer from '../Footer/Footer';
 import SearchBar from '../SearchBar/SearchBar';
 import { Link } from 'react-router-dom';
 import Paginado from '../Paginated/Paginated';
-import AddProducts from "../AddProducts/AddProducts"; 
+import AddProducts from "../AddProducts/AddProducts";
 import "./Home.css";
 
 export default function Home() {
@@ -40,7 +40,7 @@ export default function Home() {
 
   useEffect(() => {
     currentProducts.length && dispatch(getProductsByNameAndFilters(search, filters));
-  }, [dispatch, filters,currentProducts,search]);
+  }, [dispatch, filters, currentProducts, search]);
   const [orden, setOrden] = useState("");
 
   function handleReload(e) {
@@ -69,52 +69,54 @@ export default function Home() {
         <NavBar />
       </div>
       <div>
-        
+
         <button onClick={(e) => handleReload(e)}>↻</button>
-        
+
+      </div>
+      <div className="select">
+        <div class='d-flex'>
+          <select class="form-select" aria-label="Default select example" name="ram" onChange={(e) => handleFilter(e)}>
+            <option hidden>Filtrar por RAM</option>
+            <option value="2">2 GB</option>
+            <option value="3">3 GB</option>
+            <option value="4">4 GB</option>
+            <option value="6">6 GB</option>
+            <option value="8">8 GB</option>
+            <option value="16">16 GB</option>
+            <option value="32">32 GB</option>
+            <option value="64">64 GB</option>
+          </select>
+          <select class="form-select" aria-label="Default select example" name="capacity" onChange={(e) => handleFilter(e)}>
+            <option hidden>Almacenamiento</option>
+            <option value="32">32 GB</option>
+            <option value="64">64 GB</option>
+            <option value="120">120 GB</option>
+            <option value="128">128 GB</option>
+            <option value="240">240 GB</option>
+            <option value="256">256 GB</option>
+            <option value="400">400 GB</option>
+            <option value="480">480 GB</option>
+            <option value="512">512 GB</option>
+            <option value="1">1 TB</option>
+          </select>
+          <select class="form-select" aria-label="Default select example" name="category" onChange={(e) => handleFilter(e)}>
+            <option hidden>Categoria</option>
+            <option value="Notebooks">Notebooks</option>
+            <option value="Tablets">Tablets</option>
+            <option value="Phones">Celulares</option>
+          </select>
+          <select class="form-select" aria-label="Default select example" onChange={(e) => handleSort(e)}>
+            <option hidden>Orden alfabético</option>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+          </select>
+        </div>
       </div>
       <div>
-        <select class="form-select" width='32' name="ram" onChange={(e) => handleFilter(e)}>
-          <option hidden>Filter by RAM</option>
-          <option value="2">2 GB</option>
-          <option value="3">3 GB</option>
-          <option value="4">4 GB</option>
-          <option value="6">6 GB</option>
-          <option value="8">8 GB</option>
-          <option value="16">16 GB</option>
-          <option value="32">32 GB</option>
-          <option value="64">64 GB</option>
-        </select>
-        <select name="capacity" onChange={(e) => handleFilter(e)}>
-          <option hidden>Almacenamiento</option>
-          <option value="32">32 GB</option>
-          <option value="64">64 GB</option>
-          <option value="120">120 GB</option>
-          <option value="128">128 GB</option>
-          <option value="240">240 GB</option>
-          <option value="256">256 GB</option>
-          <option value="400">400 GB</option>
-          <option value="480">480 GB</option>
-          <option value="512">512 GB</option>
-          <option value="1">1 TB</option>
-        </select>
-        <select name="category" onChange={(e) => handleFilter(e)}>
-          <option hidden>Categoria</option>
-          <option value="Notebooks">Notebooks</option>
-          <option value="Tablets">Tablets</option>
-          <option value="Phones">Celulares</option>
-        </select>
-        <select onChange={(e) => handleSort(e)}>
-          <option hidden>Orden alfabético</option>
-          <option value="A-Z">A-Z</option>
-          <option value="Z-A">Z-A</option>
-        </select>
-      </div>
-      <div>
-        <SearchBar
+        {/* <SearchBar
           setCurrentPage={setCurrentPage}
           setProductsPerPage={setProductsPerPage}
-        />
+        /> */}
       </div>
       <Paginado
         productsPerPage={productsPerPage}
@@ -123,38 +125,41 @@ export default function Home() {
         changePage={changePage}
         currentPage={currentPage}
       />
-      <div class="container text-center">
-        <div class="row">
-        <div class="col">
-        {currentProducts.length ? (
-          currentProducts.map((s) => {
-            return (
-              <>
-                <Link
-                  key={s.id}
-                  to={`/products/${s.category.toLowerCase()}/${s.id}`}
-                >
-                  <Cards
-                    model={s.model}
-                    image={s.image}
-                    brand={s.brand}
-                    id={s.id}
-                    price={s.price[0]}
-                    category={s.category.toLowerCase()}
-                  />
-                </Link>
-                <AddProducts id={s.id} />
-              </>
-            );
-          })
-        ) : (
+      <div>
+        <div>
           <div>
-            <h1>Loading...</h1>
+            {currentProducts.length ? (
+              currentProducts.map((s) => {
+                return (
+                  <>
+                    <Link
+                      key={s.id}
+                      to={`/products/${s.category.toLowerCase()}/${s.id}`}
+                    >
+                      <Cards
+                        model={s.model}
+                        image={s.image}
+                        brand={s.brand}
+                        id={s.id}
+                        inches={s.inches}
+                        operative_system={s.operative_system}
+                        capacity={s.capacity}
+                        price={s.price[0]}
+                        category={s.category.toLowerCase()}
+                      />
+                    </Link>
+                    <AddProducts id={s.id} />
+                  </>
+                );
+              })
+            ) : (
+              <div>
+                <h1>Loading...</h1>
+              </div>
+            )}
           </div>
-        )}
         </div>
-        </div>
-        
+
       </div>
       <hr />
       <Footer />
