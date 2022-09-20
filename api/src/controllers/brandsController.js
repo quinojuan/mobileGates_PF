8237fa3bb0a5f833e13d3911c49c956e9a20c6b5
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2022-09-20 13:33:26 -0300

    fixed brands route

--- api/src/controllers/brandsController.js
+++ api/src/controllers/brandsController.js
@@ -3,9 +3,7 @@ const { Brand } = require('../db.js');
 const getAllBrands = async (req, res) => {
 	try {
 		let allBrands = await Brand.findAll({ attributes: ['name'] });
-		if (allBrands.length === 0)
-			return res.status(404).json({ message: 'Error en find all' });
-			allBrands = allBrands.map((brand) => brand.name);
+		allBrands = allBrands.map((brand) => brand.name);
 		res.status(200).json(allBrands);
 	} catch (e) {
 		console.log(e);
