6ae9f1e1d781db5f4eab26eb7c4ca11e50fec75b
giuliano-627 <f15ur4.97@gmail.com>
2022-09-20 13:03:46 -0300

    Fixed redirect by landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,9 +1,10 @@
+import { useEffect } from "react";
 import { useNavigate  } from "react-router-dom";
 export default function Landing(){
     const navigate = useNavigate();
-    function handleNavigate(){
-        return(navigate("/home"))
-    }
-    handleNavigate()
+    useEffect(()=>{
+        navigate('/home')
+    },[navigate])
+    console.log("Ya paso el navigate")
     return(null)
 }
