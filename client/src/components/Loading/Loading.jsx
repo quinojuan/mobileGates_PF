2526e7b45640b73b13e9b15812fb2e006aafc9e3
Rodrigo260987 <leo_lezca@hotmail.com>
2022-09-22 15:32:37 -0300

    client borrado

--- client/src/components/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LoadingPage() {
-    return (
-        <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    )
-}
