39bf34069b2de8c71b00f1025ae2795692c8c5f8
giuliano-627 <f15ur4.97@gmail.com>
2022-09-23 11:32:23 -0300

    Still fixing branch

--- api/src/routes/capacityRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { getAllCapacities } = require('../controllers/capacitiesController');
-
-router.get('/', getAllCapacities);
-
-module.exports = router;
