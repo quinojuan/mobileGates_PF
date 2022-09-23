c2ebcb1a958d8b8dc9c0215e4203300ac89b730f
giuliano-627 <f15ur4.97@gmail.com>
2022-09-23 10:58:13 -0300

    Continuando validaciones

--- client/src/components/FormPurchase/FormPurchase.jsx
+++ client/src/components/FormPurchase/FormPurchase.jsx
@@ -9,51 +9,21 @@ import Swal from "sweetalert2";
 export default function FormPurchase() {
 
 
-    // Validates 
 
-    function validate() {
-        let errors = {};
-        if (input.email === "") {
-            errors.email = "Email es requerido!";
 
-        } else if (input.email.length < 3) {
-            errors.email = "Minimo 3 letras."
-
-        } else if (!input.creditCard) {
-            errors.creditCard = "Credit card debe estar completo.";
-            
-        }  
-         else if (input.creditCard.length >10 ) {
-            errors.creditCard = "Maximo 10 numeros.";
-
-        } else if (input.dni === "") {
-            errors.email = "Dni es requerido!";
-
-        } else if (input.dni.length>8) {
-            errors.dni = "Maximo 8 numeros."
-
-        } else if (input.adress === "") {
-            errors.adress = "Dirección requerida!"
-
-        } else if (input.birthday==="") {
-            errors.birthday = "Fecha de nacimiento es requerida."
-
-        } 
-        
-        return errors;
-    }
+    
 
 
 
 
 
-    // Validates 
-    const carts= useSelector((state)=>state.cart)
+    const carts = useSelector((state)=>state.cart)
     const dispatch = useDispatch()
     // const purchase = useSelector(state => state.purchase)
     
     let navigate = useNavigate();
     const [errors, setErrors] = useState({})
+    const [hayError,setHayError] = use(false);
     const [input, setInput] = useState({
         email: "",
         creditCard: "",
@@ -68,28 +38,14 @@ export default function FormPurchase() {
             ...input,
             [e.target.name]: e.target.value
         })
-        setErrors(validate({
-            ...input,
-            [e.target.value]: e.target.value
-        }))
-        //  console.log(input)
     }
 
-    // const handleSelect = (e) => {
-    //     setErrors(validate({
-    //         ...input,
-    //         [e.target.value]: e.target.value
-    //     }))
-    //     //  console.log(input)
-    // }
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setErrors(validate(input));
-        const errorSave = validate(input);
-        if (Object.values(errorSave).length !== 0) {
-            Swal.fire("No pudimos realizar la compra, fijese los requisitos pedidos")
+        if(hayError){
+            console.log(null);
         } else {
             dispatch(postPurchase(input))
             Swal.fire("Compra realizada")
