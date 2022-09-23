a054c9c6ad80f8231ce3738127230b9ad2eca87d
matiaskoziupa <matikoziupa@gmail.com>
2022-09-22 23:34:18 -0300

    validaciones y bugs

--- client/src/components/Details/DetailsPhone.jsx
+++ client/src/components/Details/DetailsPhone.jsx
@@ -34,7 +34,6 @@ export default function DetailsPhone(props) {
     <div>
       {
         <div>
-          <NavBar />
           <div
             className="container"
             style={{
--- client/src/components/FormPurchase/FormPurchase.jsx
+++ client/src/components/FormPurchase/FormPurchase.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getPurchase, postPurchase } from "../../redux/Actions";
 import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
+import NavBar from "../NavBar/NavBar";
 
 
 export default function FormPurchase() {
@@ -13,26 +14,23 @@ export default function FormPurchase() {
 
     function validate() {
         let errors = {};
-        if (input.email === "") {
-            errors.email = "Email es requerido!";
-
-        } else if (input.email.length < 3) {
-            errors.email = "Minimo 3 letras."
-
-        } else if (!input.creditCard) {
+        if (!input.creditCard) {
             errors.creditCard = "Credit card debe estar completo.";
             
         }  
-         else if (input.creditCard.length >10 ) {
-            errors.creditCard = "Maximo 10 numeros.";
+         else if (input.creditCard.length !==16 ) {
+            errors.creditCard = "Deben ser 16 números.";
 
         } else if (input.dni === "") {
-            errors.email = "Dni es requerido!";
+            errors.dni = "Dni es requerido!";
 
         } else if (input.dni.length>8) {
             errors.dni = "Maximo 8 numeros."
 
-        } else if (input.adress === "") {
+        } else if(!Number(input.dni)){
+            errors.dni="Debe ser un numero"
+        } 
+        else if (input.adress === "") {
             errors.adress = "Dirección requerida!"
 
         } else if (input.birthday==="") {
@@ -88,6 +86,17 @@ export default function FormPurchase() {
         e.preventDefault()
         setErrors(validate(input));
         const errorSave = validate(input);
+        if(!input.email){
+            Swal.fire("Ingrese un email valido")
+        } else if(input.creditCard.length!==16){
+            Swal.fire("La tarjeta de credito debe tener 16 dígitos.")
+        } else if(input.dni.length!==8 || input.dni.length!==7){
+            Swal.fire("El DNI debe tener 7 u 8 digitos sin puntos.")
+        } else if(!input.adress.length){
+            Swal.fire("Debes completar tu dirección.")
+        } else if(!input.birthday.length){
+            Swal.fire("Debe completar con su fecha de nacimiento.")
+        }
         if (Object.values(errorSave).length !== 0) {
             Swal.fire("No pudimos realizar la compra, fijese los requisitos pedidos")
         } else {
@@ -114,6 +123,8 @@ export default function FormPurchase() {
 
     if(carts.length){
     return (
+        <div>
+        <NavBar/>
         <div >
             <Link to="/home">
                 <span>back</span>
@@ -129,11 +140,8 @@ export default function FormPurchase() {
                         name="email"
                         onChange={(e)=>handleChange(e)}
                     />
-                    {
-                        errors.email && (
-                            <p >{errors.email}</p>
-                        )
-                    }
+                    
+
                 </div>
 
                 <label>Tarjeta de credito</label>
@@ -209,11 +217,13 @@ export default function FormPurchase() {
         
 
         </div>
+        </div>
         
     )
 } else {
     return (
         <h1>Debes agregar mínimo un producto al carrito para realizar tu compra</h1>
+        
     )
 }
 }
