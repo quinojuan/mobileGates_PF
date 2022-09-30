7d0c94bc3149a341e9ec76156778045f7cd45af8
Juan Quino <quinojuan@gmail.com>
2022-09-30 09:57:21 -0300

    envio de mails

--- api/src/routes/purchasesRouter.js
+++ api/src/routes/purchasesRouter.js
@@ -5,6 +5,6 @@ const { postPurchase, getAllPurchases, purchaseMail, shippingMail } = require('.
 router.get('/', getAllPurchases);
 router.post('/', postPurchase);
 router.post('/purchasemail', purchaseMail)
-router.post('/shippingmail', shippingMail)
+router.post('/shippingmail', shippingMail) // por el momento no se está utilizando (envia cuando el producto esté en camino)
 
 module.exports = router;
