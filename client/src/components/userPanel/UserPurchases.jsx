14f201d111f0f368af13554deb0880a7be347f97
giuliano-627 <f15ur4.97@gmail.com>
2022-10-04 12:11:08 -0300

    Validaciones

--- client/src/components/userPanel/UserPurchases.jsx
+++ client/src/components/userPanel/UserPurchases.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
-import { getPurchase } from "../../redux/Actions";
+import { getPurchase, getUsers } from "../../redux/Actions";
 import { useAuth } from '../Context/authContext';
 
 export default function UserPurchases(){
@@ -15,10 +15,14 @@ export default function UserPurchases(){
     console.log(logged, "USUARIOS BIEN PERRAZOS")
     const userLogin = purchases&&purchases.filter(s=>s.email === logged.email)
     useEffect(()=>{
+        dispatch(getUsers());
         dispatch(getPurchase())
     }, [])
     return (
         <div>
+        <h1>userPurchases</h1>
+        {console.log("logged:",logged)}
+        {console.log("userLogin:",userLogin)}
         {userLogin.length ? userLogin : "Aun no hay compras"}
         <button>
             <Link to= "/userpanel">Volver</Link>
