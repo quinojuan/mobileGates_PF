6cd0682c0a9a809d164673ca6cb24972887189d9
Juan Quino <quinojuan@gmail.com>
2022-10-05 21:22:40 -0300

    login, loadDB con superadmin, banned users, validaciones en el back

--- client/src/components/AdminPages/ManageUser.jsx
+++ client/src/components/AdminPages/ManageUser.jsx
@@ -64,7 +64,7 @@ export default function ManageUser() {
 													type="checkbox"
 													email={user.email}
 													disabled={!user.emailVerified}
-													checked
+													checked={user.active}
 												/>
 											) : (
 												<input
