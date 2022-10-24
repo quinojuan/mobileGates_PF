c09ffc280ec0d3967dbec1a435f32262790ed798
Juan Quino <quinojuan@gmail.com>
2022-10-24 17:10:05 -0300

    acomodando el proyecto

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} = process.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1xMCuaRWXH2GpMtdjWa_8_WwuFs4e5-w",
-  authDomain: "proyecto-e64bf.firebaseapp.com",
-  databaseURL: "https://proyecto-e64bf-default-rtdb.firebaseio.com",
-  projectId: "proyecto-e64bf",
-  storageBucket: "proyecto-e64bf.appspot.com",
-  messagingSenderId: "214423963034",
-  appId: "1:214423963034:web:946426b252468df3116623"
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+export const auth = getAuth(app);
